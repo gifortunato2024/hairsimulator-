cd9b55588e5500da5a5404ef19a8305b4d0bc03a
migueelmartinezzz <maartinez.miguel476@gmail.com>
2024-05-22 14:54:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ st.title('Hair Revolution')
 st.sidebar.header('Hair Revolution')
 st.sidebar.write("O app que vai transformar a sua forma de cuidar do cabelo")
 
-foto = Image.open('ariana.hair.jpg')
+foto = Image.open('ariana.hair.jpg', 'download.jpg', 'download(1).jpg') 
 st.image(foto, caption='Logo do Streamlit', use_column_width=False)
