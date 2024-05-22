0c506965edc5cca1f4c76d68be4854946db1cb96
migueelmartinezzz <maartinez.miguel476@gmail.com>
2024-05-22 15:30:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,10 @@ st.sidebar.write("O app que vai transformar a sua forma de cuidar do cabelo")
 foto = Image.open('camz.jpeg') 
 st.image(foto, caption="Camila Cabello L'Oreal", width= 500)
 tab1, tab2, tab3 = st.tabs(["Tipo de cabelo", "Cor do cabelo", "Procedimentos"])
-
-with tab1:
-   st.header("Qual o seu tipo de cabelo?")
-   st.image("tiposdecabelo.png", width=300)
-
-with tab2:
-   st.header("Qual a cor do seu cabelo?")
-   st.image("coresdecabelo.png", width=300)
-
-with tab3:
-   st.header("Quais procedimentos você já realizou?")
-   st.image("virgem.jpg", width=200)
+st.header("Tipo de cabelo")
+st.image("tiposdecabelo.png", width=300)
+st.radio("Qual o seu tipo de cabelo?", ['1', '2A', '2B'])
+st.header("Qual a cor do seu cabelo?")
+st.image("coresdecabelo.png", width=300)
+st.header("Quais procedimentos você já realizou?")
+st.image("virgem.jpg", width=200)
