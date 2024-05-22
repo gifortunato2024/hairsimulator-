5cd39d1f46a2671db0bfe40a9085a146621afb62
migueelmartinezzz <maartinez.miguel476@gmail.com>
2024-05-22 14:41:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st 
 st.title('hairrevolution') 
 st.header('O aplicativo que revoluciona a forma de cuidar do cabelo')
+from PIL import Image
+foto = Image.open('ariana.hair.jpg')
+st.image(foto,
+         caption='Logo do Streamlit',
+         use_column_width=False)
