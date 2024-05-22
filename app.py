8149c56b1a968afb1eae400233c2a5c8bbbc9e93
migueelmartinezzz <maartinez.miguel476@gmail.com>
2024-05-22 15:20:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,17 @@ st.title('Hair Revolution')
 st.sidebar.header('Hair Revolution')
 st.sidebar.write("O app que vai transformar a sua forma de cuidar do cabelo")
 foto = Image.open('camz.jpeg') 
-st.image(foto, caption="Camila Cabello L'oreal", width= 500)
+st.image(foto, caption="Camila Cabello L'Oreal", width= 500)
+tab1, tab2, tab3 = st.tabs(["Tipo de cabelo", "Cor do cabelo", "Procedimentos"])
+
+with tab1:
+   st.header("Qual o seu tipo de cabelo?")
+   st.image("tiposdecabelo.png", width=300)
+
+with tab2:
+   st.header("Qual a cor do seu cabelo?")
+   st.image("coresdecabelo.png", width=300)
+
+with tab3:
+   st.header("Quais procedimentos você já realizou?")
+   st.image("virgem.jpg", width=200)
