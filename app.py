a5e7451171a88fa5e071c4b6a25baa5e9d2cb6f4
gifortunato2024 <168582782+gifortunato2024@users.noreply.github.com>
2024-05-27 15:17:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ st.title('Hair Revolution')
 
 
 st.sidebar.header('Hair Revolution')
-st.sidebar.write("O app que vai transformar a sua forma de cuidar do cabelo")
+st.sidebar.write("O app que vai transformar a sua forma de cuidar do cabelo.
+Se você é um(a) amante da LÓreal professional esse é o seu site ideal")
 foto = Image.open('camz.jpeg') 
 st.image(foto, caption="Camila Cabello L'Oreal", width= 500)
 
