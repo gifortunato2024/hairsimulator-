6a55289e024c8e6ef930782793aa8c5fdc4980fd
gifortunato2024 <168582782+gifortunato2024@users.noreply.github.com>
2024-06-06 15:30:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ translator = GoogleTranslator(source='pt', target='en')
 
 st.title('Hair Revolution')
 
-
 st.sidebar.header('Hair Revolution')
 st.sidebar.write("O app que vai transformar a sua forma de cuidar do cabelo. Se você é um(a) amante da LÓreal professional esse é o seu site ideal")
 foto = Image.open('camz.jpeg') 
@@ -39,29 +38,28 @@ with st.form('form'):
     opções_cabelo = st.radio("Selecione as seguintes opções que o seu cabelo está aparentando", ['Ponta dupla', 'Frizz', 'Poroso', 'Queda', 'Quebra', 'Crescimento tardio', 'Caspa'],
                             horizontal=True) 
 
-
     st.subheader("Linhas L'Oréal Professionel")
-st.image("Linhassite .jpg", width=400)
+    st.image("Linhassite .jpg", width=400)
 
-descricoes_linhas = {
-    'Absolut Repair Molecular': 'Reparação profunda e reconstrução para cabelos danificados.',
-    'Metal Detox': 'Proteção contra poluentes e impurezas.',
-    'Scalp Advanced': 'Cuidados avançados para o couro cabeludo, combatendo problemas como caspa e oleosidade.',
-    'Curl Expression': 'Cuidados especiais para cabelos cacheados, proporcionando definição e controle de frizz.',
-    'Absolut Repair': 'Reparação e nutrição para cabelos danificados, restaurando a saúde e o brilho.',
-    'Choma Creme': 'Tratamento para cabelos coloridos, protegendo a cor e prolongando a intensidade.',
-    'Fluidfier': 'Controle de frizz e definição de cabelos lisos.',
-    'Pro Longer': 'Cuidados para cabelos mais longos, fortalecendo e protegendo as pontas.',
-    'Blondifier': 'Cuidados específicos para cabelos loiros, neutralizando tons amarelados e proporcionando brilho.',
-    'Inforce': 'Fortalecimento e reconstrução para cabelos enfraquecidos e quebradiços.',
-    'Nutrifier': 'Nutrição intensa para cabelos secos e desidratados.'
-}
+    descricoes_linhas = {
+        'Absolut Repair Molecular': 'Reparação profunda e reconstrução para cabelos danificados.',
+        'Metal Detox': 'Proteção contra poluentes e impurezas.',
+        'Scalp Advanced': 'Cuidados avançados para o couro cabeludo, combatendo problemas como caspa e oleosidade.',
+        'Curl Expression': 'Cuidados especiais para cabelos cacheados, proporcionando definição e controle de frizz.',
+        'Absolut Repair': 'Reparação e nutrição para cabelos danificados, restaurando a saúde e o brilho.',
+        'Choma Creme': 'Tratamento para cabelos coloridos, protegendo a cor e prolongando a intensidade.',
+        'Fluidfier': 'Controle de frizz e definição de cabelos lisos.',
+        'Pro Longer': 'Cuidados para cabelos mais longos, fortalecendo e protegendo as pontas.',
+        'Blondifier': 'Cuidados específicos para cabelos loiros, neutralizando tons amarelados e proporcionando brilho.',
+        'Inforce': 'Fortalecimento e reconstrução para cabelos enfraquecidos e quebradiços.',
+        'Nutrifier': 'Nutrição intensa para cabelos secos e desidratados.'
+    }
 
-linha = st.radio("Escolha a linha que você quer ver o resultado no seu cabelo", list(descricoes_linhas.keys()))
+    linha = st.radio("Escolha a linha que você quer ver o resultado no seu cabelo", list(descricoes_linhas.keys()))
 
-# Exibir descrição ao lado do radio button
-if linha in descricoes_linhas:
-    st.write("Descrição:", descricoes_linhas[linha])
+    # Exibir descrição ao lado do radio button
+    if linha in descricoes_linhas:
+        st.write("Descrição:", descricoes_linhas[linha])
 
     botao = st.form_submit_button('enviar') 
 
